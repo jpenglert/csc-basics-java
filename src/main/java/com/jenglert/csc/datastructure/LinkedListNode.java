a984f852bffcd5d4e5bfeac123623f7658cee868
Jesse Englert <jesse.englert@hciq.com>
2019-06-11 17:47:37 -0700

    Add LinkedListNode data structure

---
+++ src/main/java/com/jenglert/csc/datastructure/LinkedListNode.java
@@ -0,0 +1,29 @@
+package com.jenglert.csc.datastructure;
+
+/**
+ * Adapted from "Cracking the Coding Interview, 6th Edition" p649 for use in practise problems
+ */
+public class LinkedListNode {
+    public LinkedListNode next;
+    public LinkedListNode prev;
+    public int data;
+
+    public LinkedListNode(int d) {
+        data = d;
+    }
+
+    public void setNext(LinkedListNode n) {
+        next = n;
+
+        if (n != null && n.prev != this) {
+            n.setPrevious(this);
+        }
+    }
+
+    public void setPrevious(LinkedListNode p) {
+        prev = p;
+        if (p != null && p.next != this) {
+            p.setNext(this);
+        }
+    }
+}
